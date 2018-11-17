bef215bda97ec3e16ab24da6370aa3cbcfca5334
casey-boyer <casey-boyer@users.noreply.github.com>
2018-11-16 18:45:31 -0600

    Trying to add an instrument class

---
+++ cozmo-song-match-master/song_match/song/instrument.py
@@ -0,0 +1,8 @@
+class Instrument:
+    def __init__(self):
+        # Piano instrument by default #
+        self.instrument = "Piano"
+
+    def get_instrument(self) -> str:
+        # Randomly return an instrument #
+        return "Piano"
