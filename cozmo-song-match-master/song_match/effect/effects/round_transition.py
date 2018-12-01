ac09dcb3c198ab353e7ade6519a6c355e1b2c4f5
casey-boyer <casey-boyer@users.noreply.github.com>
2018-12-01 17:45:18 -0600

    Added a 'play again' feature so the user may keep playing.

--- cozmo-song-match-master/song_match/effect/effects/round_transition.py
+++ cozmo-song-match-master/song_match/effect/effects/round_transition.py
@@ -19,7 +19,7 @@ class RoundTransitionEffect(Effect):
         :return: None
         """
         play_level_complete_sound()
-        await self._song_robot.turn_back_to_center(in_parallel=True)
         await self._song_robot.say_text(COZMO_NEXT_ROUND).wait_for_completed()
+        await self._song_robot.turn_back_to_center(in_parallel=True)
         await self._note_cubes.start_and_stop_light_chasers()
         await sleep(1)
