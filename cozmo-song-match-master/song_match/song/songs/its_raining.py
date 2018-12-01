8908e737650907d9cd4866d662ea890fd10f803c
Shaon Islam <sidw8@mail.umsl.edu>
2018-12-01 15:45:42 -0600

    raining song fixed

--- cozmo-song-match-master/song_match/song/songs/its_raining.py
+++ cozmo-song-match-master/song_match/song/songs/its_raining.py
@@ -27,19 +27,22 @@ class ItsRaining(Song):
 
     @property
     def _notes(self) -> List[Note]:
+        instrument = self.get_instrument().get_instrument_str()
         return [
-            Note('E4'),
-            Note('G4'),
-            Note('A4')
+            Note('E4', instrument),
+            Note('G4', instrument),
+            Note('A4', instrument)
         ]
 
     @property
     def _sequence(self) -> List[Note]:
-        a_quarter = Note('C4', QUARTER_NOTE)
-        g_quarter = Note('D4', QUARTER_NOTE)
-        e_quarter = Note('E4', QUARTER_NOTE)
-        g_half = Note('D4', HALF_NOTE)
-        e_half = Note('E4', HALF_NOTE)
+        # Defining notes for it's raining song
+        instrument = self.get_instrument().get_instrument_str()
+        a_quarter = Note('C4', instrument, QUARTER_NOTE)
+        g_quarter = Note('D4', instrument, QUARTER_NOTE)
+        e_quarter = Note('E4', instrument, QUARTER_NOTE)
+        g_half = Note('D4', instrument, HALF_NOTE)
+        e_half = Note('E4', instrument, HALF_NOTE)
         return [
             e_quarter, g_half, e_quarter, a_quarter, g_half,
             e_quarter, e_quarter, g_half, e_quarter, a_quarter,
